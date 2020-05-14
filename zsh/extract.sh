0c3697ce0741ff2ccc4232bc683c9e86c92052ab
Chad Effler <Spicychckn@gmail.com>
2020-05-14 07:20:37 -0400

    Add Zsh config files

---
+++ zsh/extract.sh
@@ -0,0 +1,19 @@
+extract() {
+  if [ -f $1 ] ; then
+    case $1 in
+      *.tar.gz)  tar xzf $1 ;;
+      *.tar.bz2) tar xjf $1 ;;
+      *.bz2)     bunzip2 $1 ;;
+      *.rar)     rar x   $1 ;;
+      *.gz)      gunzip  $1 ;;
+      *.tar)     tar xf  $1 ;;
+      *.tbz2)    tar xjf $1 ;;
+      *.tgz)     tar xzf $1 ;;
+      *.zip)     unzip   $1 ;;
+      *.z)    uncompress $1 ;;
+      *)      echo "'$1' cannot be extracted via extract()" ;;
+    esac
+  else
+    echo "'$1' is not a valid file"
+  fi
+}
