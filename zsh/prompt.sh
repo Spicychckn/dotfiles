f66d6a91cd329dadbc4f45ecd1a9fb12173dec23
Chad Effler <Spicychckn@gmail.com>
2020-05-17 01:31:41 -0400

    Update zsh prompt with better colors and host

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -1,6 +1,6 @@
 # Reference for colors: http://stackoverflow.com/questions/689765/how-can-i-change-the-color-of-my-prompt-in-zsh-different-from-normal-text
 
-autoload -U colors && colors
+#autoload -U colors && colors
 
 setopt PROMPT_SUBST
 
@@ -8,20 +8,35 @@ set_prompt() {
 
 
 	# [
-	PS1="%{$fg[white]%}[%{$reset_color%}"
+	PS1="%B%{%F{white}%}["
 
-	# Path: http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
-	PS1+="%{$fg_bold[cyan]%}${PWD/#$HOME/~}%{$reset_color%}"
+	# Sudo
+  if (! $_no_sudo)
+    then
+	  CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1|grep "load"|wc -l)
+	  if [ ${CAN_I_RUN_SUDO} -gt 0 ]
+	  then
+		  PS1+="%{$fg_bold[red]%}SUDO %{$reset_color%}"
+	  fi
+  fi
 
-	# Status Code
-	PS1+='%(?.., %{$fg[red]%}%?%{$reset_color%})'
+  # Username
+  PS1+="%{%F{026}%}%n"
+
+  # Host
+  PS1+="%{%F{026}%}@"
+  PS1+="%{%F{026}%}%M:"
+
+	# Path
+	PS1+="%{%F{cyan}%}%~"
+
  
   #GIT
   if [ ! $_no_git ];
   then
 	  if git rev-parse --is-inside-work-tree 2> /dev/null | grep -q 'true' ; then
 		  PS1+=', '
-		  PS1+="%{$fg[blue]%}$(git rev-parse --abbrev-ref HEAD 2> /dev/null)%{$reset_color%}"
+		  PS1+="%{%F{208}%}$(git rev-parse --abbrev-ref HEAD 2> /dev/null)%{$reset_color%}"
 		  if [ $(git status --short 2> /dev/null 2> /dev/null | wc -l) -gt 0 ]; then 
 			  PS1+="%{$fg[red]%}+$(git status --short 2> /dev/null | wc -l | awk '{$1=$1};1')%{$reset_color%}"
 		  fi
@@ -29,7 +44,7 @@ set_prompt() {
   fi
 
 
-	# Timer: http://stackoverflow.com/questions/2704635/is-there-a-way-to-find-the-running-time-of-the-last-executed-command-in-the-shel
+	# Timer
 	if [[ $_elapsed[-1] -ne 0 ]]; then
 		PS1+=', '
 		PS1+="%{$fg[magenta]%}$_elapsed[-1]s%{$reset_color%}"
@@ -41,18 +56,10 @@ set_prompt() {
 		PS1+="%{$fg[yellow]%}PID:$!%{$reset_color%}"
 	fi
 
-	# Sudo: https://superuser.com/questions/195781/sudo-is-there-a-command-to-check-if-i-have-sudo-and-or-how-much-time-is-left
-  if [ ! $_no_sudo ];
-  then
-	  CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1|grep "load"|wc -l)
-	  if [ ${CAN_I_RUN_SUDO} -gt 0 ]
-	  then
-		  PS1+=', '
-		  PS1+="%{$fg_bold[red]%}SUDO%{$reset_color%}"
-	  fi
-  fi
+	# Status Code
+	PS1+='%(?.., %{$fg[red]%}%?%{$reset_color%})'
 
-	PS1+="%{$fg[white]%}]: %{$reset_color%}% "
+	PS1+="%{%F{white} ]%{$reset_color%}: "
 }
 
 precmd_functions+=set_prompt
