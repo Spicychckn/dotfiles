8013b62a7d7423e8289c01192f8efe58fc464c59
Chad Effler <Spicychckn@gmail.com>
2020-05-17 23:59:37 -0400

    Change prompt.sh to use Zsh style color format
    change calls to $fg() and $reset_color()
                 to %F{}  and %f

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -8,7 +8,7 @@ set_prompt() {
 
 
 	# [
-	PS1="%B%{%F{white}%}["
+	PS1="%B%{%F{white}%}[%{%f%}"
 
 	# Sudo
   if (! $_no_sudo)
@@ -16,19 +16,19 @@ set_prompt() {
 	  CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1|grep "load"|wc -l)
 	  if [ ${CAN_I_RUN_SUDO} -gt 0 ]
 	  then
-		  PS1+="%{$fg_bold[red]%}SUDO %{$reset_color%}"
+		  PS1+="%{%F{red}%}SUDO %{%f%}"
 	  fi
   fi
 
   # Username
-  PS1+="%{%F{026}%}%n"
+  PS1+="%{%F{026}%}%n%{%f%}"
 
   # Host
-  PS1+="%{%F{026}%}@"
-  PS1+="%{%F{026}%}%m:"
+  PS1+="%{%F{026}%}@%{%f%}"
+  PS1+="%{%F{026}%}%m:%{%f%}"
 
 	# Path
-	PS1+="%{%F{cyan}%}%~"
+	PS1+="%{%F{cyan}%}%~%{%f%}"
 
  
   #GIT
@@ -36,9 +36,9 @@ set_prompt() {
   then
 	  if git rev-parse --is-inside-work-tree 2> /dev/null | grep -q 'true' ; then
 		  PS1+=', '
-		  PS1+="%{%F{208}%}$(git rev-parse --abbrev-ref HEAD 2> /dev/null)%{$reset_color%}"
+		  PS1+="%{%F{208}%}$(git rev-parse --abbrev-ref HEAD 2> /dev/null)%{%f%}"
 		  if [ $(git status --short 2> /dev/null 2> /dev/null | wc -l) -gt 0 ]; then 
-			  PS1+="%{$fg[red]%}+$(git status --short 2> /dev/null | wc -l | awk '{$1=$1};1')%{$reset_color%}"
+			  PS1+="%{%F{red}%}+$(git status --short 2> /dev/null | wc -l | awk '{$1=$1};1')%{%f%}"
 		  fi
 	  fi
   fi
@@ -47,19 +47,19 @@ set_prompt() {
 	# Timer
 	if [[ $_elapsed[-1] -ne 0 ]]; then
 		PS1+=', '
-		PS1+="%{$fg[magenta]%}$_elapsed[-1]s%{$reset_color%}"
+		PS1+="%{%F{magenta}%}$_elapsed[-1]s%{%f%}"
 	fi
 
 	# PID
 	if [[ $! -ne 0 ]]; then
 		PS1+=', '
-		PS1+="%{$fg[yellow]%}PID:$!%{$reset_color%}"
+		PS1+="%{%F{yellow}%}PID:$!%{%f%}"
 	fi
 
 	# Status Code
-	PS1+='%(?.., %{$fg[red]%}%?%{$reset_color%})'
+	PS1+="%(?.., %{%F{red}%}%?%{%f%})"
 
-	PS1+="%{%F{white} ]%{$reset_color%}: "
+	PS1+="%{%F{white} ]%b%{%f%}: "
 }
 
 precmd_functions+=set_prompt
