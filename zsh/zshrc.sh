63450e43b9aa6dbe92cf6d441f3f67a4cd1c794b
Chad Effler <Spicychckn@gmail.com>
2020-05-18 07:17:40 -0400

    Update du1 and du2 to output sorted results

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -15,8 +15,8 @@
 	alias ....="cd ../../.."
 	alias c="clear"
   alias la="ls -lah"
-  alias du1="du -h -d1"
-  alias du2="du -h -d2"
+  alias du1="du -h -d1 | sort -k2"
+  alias du2="du -h -d2 | sort -k2"
 	mkdir -p /tmp/log
 	
 	# This is currently causing problems (fails when you run it anywhere that isn't a git project's root directory)
